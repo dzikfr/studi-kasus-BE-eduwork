94ff0b8789e5a753c74714524fcdd199d6725815
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-09-16 17:11:38 +0700

    fix auth register api

--- app/user/model.js
+++ app/user/model.js
@@ -1,13 +1,12 @@
-// const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
-const {model, Schema} = mongoose;
+const { model, Schema } = mongoose;
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 const bcrypt = require('bcrypt');
 
 let userSchema = Schema({
     full_name: {
-        type: String, 
-        minlength: [3, 'Panjang nama makanan minimal 3 karakter'],
+        type: String,
+        minlength: [3, 'Panjang nama minimal 3 karakter'],
         maxlength: [255, 'Panjang nama maksimal 255 karakter'],
         required: [true, 'Nama harus diisi']
     },
@@ -19,12 +18,13 @@ let userSchema = Schema({
     email: {
         type: String,
         required: [true, 'Email harus diisi'],
-        maxlength: [255, 'Panjang email maksimal 255 karakter']
+        maxlength: [255, 'Panjang email maksimal 255 karakter'],
+        unique: true
     },
 
     password: {
         type: String,
-        required: [true, '{Password harus diisi'],
+        required: [true, 'Password harus diisi'],
         maxlength: [255, 'Panjang maksimal password 255 karakter'],
     },
 
@@ -36,28 +36,34 @@ let userSchema = Schema({
 
     token: [String]
 
-}, {timestamps: true});
+}, { timestamps: true });
 
-userSchema.path('email').validate((value)=>{
+userSchema.path('email').validate((value) => {
     const EMAIL_RE = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return EMAIL_RE.test(value);
 }, attr => `${attr.value} harus merupakan email yang valid!`);
 
-userSchema.path('email').validate(async function(value){
-    try{
-        const count = await this.model('User').count({email: value});
+userSchema.path('email').validate(async function (value) {
+    try {
+        const count = await this.model('User').countDocuments({ email: value });
         return !count;
-    }catch(err){
-        throw err
+    } catch (err) {
+        throw err;
     }
 }, attr => `${attr.value} sudah terdaftar`);
 
 const HASH_ROUND = 10;
-userSchema.pre('save', function(next){
-    this.password = bcrypt.hashSync(this.password, HASH_ROUND);
-    next()
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    try {
+        this.password = await bcrypt.hash(this.password, HASH_ROUND);
+        next();
+    } catch (err) {
+        next(err);
+    }
 });
 
-userSchema.plugin(AutoIncrement, {inc_field: 'customer_id'});
+userSchema.plugin(AutoIncrement, { inc_field: 'customer_id' });
 
 module.exports = model('User', userSchema);
